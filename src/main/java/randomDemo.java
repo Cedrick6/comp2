ca422a1f79bcd43b2ffc2ac71ce73ce2d8e1e87e
cedricklubin <clubin39@gmail.com>
2021-09-15 15:50:12 -0400

    RandomDemo Finished

--- src/main/java/randomDemo.java
+++ src/main/java/randomDemo.java
@@ -1,15 +1,28 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class randomDemo {
     public static void main(String[] args){
         var randomNumberPicker = new Random();
         var randomNumber = randomNumberPicker.nextInt(10)+1;
         var inputReader = new Scanner(System.in);
-        System.out.println("Pick a random number from 1 to 10: ");
-        var userNumber = inputReader.nextInt();
-        if(randomNumber == userNumber){
-            System.out.println();
+        System.out.print("pick a random number from 1 to 10:");
+        var usersNumber = inputReader.nextInt();
+        while(true) {
+            if (randomNumber == usersNumber) {
+                System.out.println("You were right the number was " + randomNumber);
+                break;
+            } else {
+                if(usersNumber > randomNumber)
+                    System.out.println("Unfortunately " + usersNumber +
+                            " was too high");
+                else
+                    System.out.println("Unfortunately " + usersNumber +
+                            " was too low");
+                System.out.print("Try again:");
+                usersNumber = inputReader.nextInt();
+            }
         }
     }
 }
+
+
